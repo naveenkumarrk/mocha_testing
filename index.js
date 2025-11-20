29d0286e4efdfa1460329daf92d8f7a2cd8db1da
naveenkumarrk <111naveenkumarrk@gmail.com>
2025-11-20 18:36:30 +0530

    firmly-015 : craeted a /random route

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const router = Router();
 
 router.get('/health', () => new Response('Todo Worker is running!'));
 
-router.get(
-  '/random',
-  () => new Response('random number is: ', Math.random().toString())
-);
+router.get('/random', () => {
+  const num = Math.random().toString();
+  return new Response(`random number is: ${num}`);
+});
+
 // Todos routes
 router.get('/api/todos', getTodos);
 router.get('/api/todos/:id', getTodoById);
